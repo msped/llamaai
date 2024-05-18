ef568a8e4f7b8f69e9c8c29e88059823c1822ede
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-19 00:15:05 +0100

    build: removed all auth options for new branch

--- src/lib/auth.js
+++
@@ -1,9 +0,0 @@
-import NextAuth from "next-auth";
-import GitHub from "next-auth/providers/github";
-import { db } from "@/db/schema"
-import { DrizzleAdapter } from "@auth/drizzle-adapter";
-
-export const { handlers, signIn, signOut, auth } = NextAuth({
-    adapter: DrizzleAdapter(db),
-    providers: [GitHub,],
-})
