64070d769bbcd58b9e001ce7d222937f656e6cfc
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-18 10:44:44 +0100

    feat: drizzle setup refactor and migration

--- drizzle.config.js
+++ drizzle.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from "drizzle-kit"
 
 export default defineConfig({
-    schema: "./drizzle/schema.js",
-    out: "./drizzle/migrations",
-    driver: "pg",
+    dialect: "postgresql",
+    schema: "./src/db/schema.js",
+    out: "./src/db/migrations",
     dbCredentials: {
-        connectionString: process.env.DATABASE_URL,
+        url: process.env.DATABASE_URL,
     },
     verbose: true,
     strict: true,
