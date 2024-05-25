325a786fca1ef375dd9c382b9512f9aa0b3f775a
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-25 23:58:09 +0100

    feat: added UserButton

---
+++ src/components/auth/UserButton.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { useCurrentUser } from '@/hooks/useCurrentUser';
+import { LogoutButton } from '@/components/auth/Logout';
+
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+import IconButton from '@mui/material/IconButton';
+import PersonIcon from '@mui/icons-material/Person';
+import Settings from '@/components/auth/Settings'
+
+export default function UserButton() {
+    const user = useCurrentUser();
+    const [anchorEl, setAnchorEl] = React.useState(null);
+
+    const handleClick = (event) => {
+        setAnchorEl(event.currentTarget);
+    };
+
+    const handleClose = () => {
+        setAnchorEl(null);
+    };
+
+    return (
+        <>
+            <IconButton onClick={handleClick} size="large" sx={{ ml: 2 }}>
+                <Avatar src={user?.image || ''}>
+                    {!user?.image && <PersonIcon size="small" />}
+                </Avatar>
+            </IconButton>
+            <Menu
+                anchorEl={anchorEl}
+                open={Boolean(anchorEl)}
+                onClose={handleClose}
+                MenuListProps={{
+                    'aria-labelledby': 'user-account-menu-button',
+                }}
+            >
+                <MenuItem>
+                    <Settings />
+                </MenuItem>
+                <MenuItem onClick={handleClose}>
+                    <LogoutButton />
+                </MenuItem>
+            </Menu>
+        </>
+    );
+}
