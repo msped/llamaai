a19f317fcb06d32e50618e7f100c7e2e8d991c10
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-25 23:56:18 +0100

    feat: added header to root layout

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
 import Provider from "../../Provider";
+import Header from "@/components/Header";
+import "./globals.css";
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <Provider>
         <body>
+          <Header />
           {children}
+          {/* <Footer /> */}
         </body>
       </Provider>
     </html>
