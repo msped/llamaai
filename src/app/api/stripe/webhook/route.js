1b84a8600b507c27170d0fa36184e6c7df2fe6c8
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-27 22:19:32 +0100

    feat: added stripe webhook to handle successful subscription

---
+++ src/app/api/stripe/webhook/route.js
@@ -0,0 +1,62 @@
+
+import { stripe } from "@/lib/stripe";
+import { headers } from "next/headers";
+import { subscriptions } from "@/db/schema";
+import { eq } from "drizzle-orm";
+import { db } from "@/db";
+
+export async function POST(req) {
+    const body = await req.text();
+    const signature = headers().get("Stripe-Signature");
+
+    let event;
+
+    try {
+        event = stripe.webhooks.constructEvent(
+            body,
+            signature,
+            process.env.STRIPE_WEBHOOK_SECRET
+        );
+    } catch (error) {
+        return new Response(
+            `Webhook Error: ${
+                error instanceof Error ? error.message : "Unknown error"
+            }`,
+            { status: 400 }
+        );
+    }
+
+    const session = event.data.object;
+
+    if (event.type === "checkout.session.completed") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription
+        );
+
+        await db.insert(subscriptions).values({
+            userId: session.metadata.userId,
+            stripeSubscriptionId: subscription.id,
+            stripeCustomerId: subscription.customer,
+            stripePriceId: subscription.items.data[0]?.price.id,
+            stripeCurrentPeriodEnd: new Date(
+                subscription.current_period_end * 1000
+            ).toISOString(),
+        });
+    } else if (event.type === "invoice.payment_succeeded") {
+        const subscription = await stripe.subscriptions.retrieve(
+            session.subscription
+        );
+
+        await db
+        .update(subscriptions)
+        .set({
+            stripePriceId: subscription.items.data[0]?.price.id,
+            stripeCurrentPeriodEnd: new Date(
+            subscription.current_period_end * 1000
+            ).toISOString(),
+        })
+        .where(eq(subscriptions.stripeSubscriptionId, subscription.id));
+    }
+
+    return new Response(null, { status: 200 });
+}
