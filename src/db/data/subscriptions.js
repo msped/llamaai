a68c917dfdf465b954dddc8c0242cab7f546eb14
Matt Edwards <mattsp.edwards@googlemail.com>
2024-05-31 22:40:59 +0100

    feat: added getSubscription helper function

---
+++ src/db/data/subscriptions.js
@@ -0,0 +1,10 @@
+"use server"
+
+import { db } from "@/db/index";
+import { subscriptions } from "../schema";
+import { eq } from "drizzle-orm";
+
+export async function getSubscription(userId) {
+    const subscription = await db.select().from(subscriptions).where(eq(subscriptions.userId, userId))
+    return subscription;
+}
