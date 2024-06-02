4382d111fdd7c371ffbfc305daee52a34a610103
Matt Edwards <mattsp.edwards@googlemail.com>
2024-06-02 22:24:53 +0100

    feat: added issue table helper functions

---
+++ src/octokit/data/issues.js
@@ -0,0 +1,25 @@
+"use server"
+
+import { db } from '@/db/index';
+import { eq } from 'drizzle-orm';
+import { githubIssue } from '@/db/schema';
+
+
+export const createIssue = async (data) => {
+    try {
+        const issue = await db.insert(githubIssue).create(data);
+        return issue;
+    } catch (error) {
+        return null;
+    }
+};
+
+export const updateIssue = async (id, data) => {
+    try {
+        const issue = await db.update(githubIssue).set(data).where(eq(githubIssue.id, id));
+        return issue;
+    } catch (error) {
+        return null;
+    }
+}
+
