b7713949dbaab68f5d95e3d88bc18382d1dd82a2
Matt Edwards <mattsp.edwards@googlemail.com>
2024-06-23 23:02:08 +0100

    refactor: create-issue-comment function

--- src/octokit/utils/create-issue-comment.js
+++ src/octokit/utils/create-issue-comment.js
@@ -1,15 +1,17 @@
-import getOctokit from "../octokit";
-
-export default async function createIssueComment(owner, repo, issueNumber, body) {
+export default async function createIssueComment(octokit, owner, repo, issueNumber, body) {
     try {
-        const response = await getOctokit.issues.createComment({
-            owner,
-            repo,
-            issue_number: issueNumber,
-            body
-        });
-        console.log(`Comment created: ${response.data.html_url}`)
+        const response = await octokit.request(
+            "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+            {
+                owner,
+                repo,
+                issue_number: issueNumber,
+                body
+            }
+        );
+        console.log(`Comment created: ${response}`)
     } catch (error) {
+        console.log(error)
         console.error(`Could not add comment: ${error}`)
     }
 }
