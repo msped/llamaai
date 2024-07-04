480789f38a4d5729fca1e3811a7d86fd619f04c8
Matt Edwards <mattsp.edwards@googlemail.com>
2024-07-04 22:36:28 +0100

    build: removed gemini file

--- src/gemini/gemini.js
+++
@@ -1,8 +0,0 @@
-import { GoogleGenerativeAI } from "@google/generative-ai";
-
-// Access your API key as an environment variable (see "Set up your API key" above)
-const genAI = new GoogleGenerativeAI(process.env.GOOGLE_GEMINI_API_KEY);
-
-// The Gemini 1.5 models are versatile and work with most use cases
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-pro"});
-
